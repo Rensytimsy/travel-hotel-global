bf699f58f1c16fff298fa9b79cf35d85d291aef0
Rensytimsy <rensytimsy03@gmail.com>
2024-08-30 19:34:33 +0300

    Only first page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home-page', { title: 'THG' });
+  res.render('home-page', { title: 'Travel hotel global' });
 });
 
 module.exports = router;
